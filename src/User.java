ee1a0bcf91a512fa336c2e228ddfe123dd5a7c63
nickisverygood <nickisverysmart@gmail.com>
2018-11-13 18:44:29 -0800

    ggbuh

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private String password;
+    private String city;
+
+    private BST<User> friends;
+
+    private List<String> interests;
+
+}
+
